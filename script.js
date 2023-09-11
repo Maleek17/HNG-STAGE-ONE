379af9665a28c60ff75bc68cf92a2303d475d89d
Maleek17 <omarmaleek17@gmail.com>
2023-09-11 09:47:16 +0100

    set interval

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const dayOfTheWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
 const utcTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
+function updateDateTime(){
+    const daysOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    const currentDate = new Date();
+    const currentDayIndex = currentDate.getDay();
+    const currentDay = daysOfWeek[currentDayIndex];
+    
+    dayOfTheWeekElement.textContent = currentDay;
+    
+    const currentUTCTime = new Date().toUTCString();
+    utcTimeElement.textContent = currentUTCTime;
+}
 
-const daysOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-const currentDate = new Date();
-const currentDayIndex = currentDate.getDay();
-const currentDay = daysOfWeek[currentDayIndex];
 
-dayOfTheWeekElement.textContent = currentDay;
+updateDateTime();
 
-const currentUTCTime = new Date().toUTCString();
-utcTimeElement.textContent = currentUTCTime;
+setInterval(updateDateTime, 1000);
